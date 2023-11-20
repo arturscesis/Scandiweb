3491c016de1985c655a5aee72b1ed193e77671b0
arturscesis <arturspodzins21@gmail.com>
2023-11-21 00:21:13 +0200

    slight changes

--- app/controllers/AddController.php
+++ app/controllers/AddController.php
@@ -2,7 +2,7 @@
 
 namespace App\Controllers;
 
-use App\Src\Scanditest\Product;
+use App\Src\ScandiTest\Product;
 
 class AddController {
 
--- app/src/ScandiTest/Product.php
+++ app/src/ScandiTest/Product.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Src\Scanditest;
+namespace App\Src\ScandiTest;
 
 use mysqli;
 
