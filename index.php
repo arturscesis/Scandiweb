85fdcf8ee155440fccd78c26bdbdc29543e9c371
arturscesis <arturspodzins21@gmail.com>
2023-11-21 00:12:43 +0200

    some changes

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
-// Autoloader
 require 'vendor/autoload.php';
 
 $router = new App\Router();
 
-$router->get('/', [new App\Controllers\ListController(), 'index']);
-$router->post('/delete', [new App\Controllers\ListController(), 'delete']);
-$router->get('/add', [new App\Controllers\AddController(), 'index']);
-$router->post('/store', [new App\Controllers\AddController(), 'store']);
+// Define routes
+$router->get('/scanditest/', [new App\Controllers\ListController(), 'index']);
+$router->post('/scanditest/delete', [new App\Controllers\ListController(), 'delete']);
+$router->get('/scanditest/add', [new App\Controllers\AddController(), 'index']);
+$router->post('/scanditest/store', [new App\Controllers\AddController(), 'store']);
 
 $router->dispatch();
